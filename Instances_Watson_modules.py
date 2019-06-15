f973173781b2d76388b7d65eac7aa86d66fa1009
Daniel Pouzada <33990318+dpouzada@users.noreply.github.com>
2019-06-16 01:25:15 +0200

    Update Instances_Watson_modules.py

--- Instances_Watson_modules.py
+++ Instances_Watson_modules.py
@@ -13,13 +13,13 @@ from ibm_watson.natural_language_understanding_v1 \
 # Appel de l'instance NLU
 naturalLanguageUnderstanding = NaturalLanguageUnderstandingV1(
     version='2018-11-16',
-    iam_apikey='yFVW0tXh8_yT4U75Mho6vLkP0nVEI1H-S4G8iic5aJ5i',
+    iam_apikey='xxx',
     url='https://gateway-fra.watsonplatform.net/natural-language-understanding/api'
 )
 
 #Appel de l'instance Chatbot (Assistant)
 assistant = ibm_watson.AssistantV1(
-    iam_apikey='WLskhWCSWigQdpEOoPRZUkH6Fz1cF8mcB9kb4sgS4DYK',
+    iam_apikey='xxx',
     version='2018-09-20',
     url='https://gateway-fra.watsonplatform.net/assistant/api'
 )
@@ -29,7 +29,7 @@ from ibm_watson import NaturalLanguageClassifierV1
 
 # Appel de l'instance NLC
 natural_language_classifier = NaturalLanguageClassifierV1(
-    iam_apikey='4n6XwggNiNH5MWpl11KgJ6fEX7rM72IPL0EZTCPtw0z7',
+    iam_apikey='xxx',
     url='https://gateway-fra.watsonplatform.net/natural-language-classifier/api')
 
 #Importation modules Translator
@@ -39,7 +39,7 @@ from ibm_watson import LanguageTranslatorV3
 language_translator = LanguageTranslatorV3(
 version='2018-05-01',
 url='https://gateway-fra.watsonplatform.net/language-translator/api',
-iam_apikey='6TQOpf3QV-u1dRw0j5wEVZH60ifA46mvTImkuYyEFYhG'
+iam_apikey='xxx'
 )
 
-workspace_id_assistant='6753a67d-ae39-4ef5-be4e-751e35aab09e'
+workspace_id_assistant='xxx'
